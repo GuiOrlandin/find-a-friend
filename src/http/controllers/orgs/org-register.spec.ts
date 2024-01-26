4dd3d39d21e72a0535deecc9f8e380969a8f7206
Guilherme <guiorlandin@gmail.com>
2024-01-26 13:54:23 -0300

    test environment implemented

---
+++ src/http/controllers/orgs/org-register.spec.ts
@@ -0,0 +1,27 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "../../../app";
+
+describe("Org Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register a org", async () => {
+    const response = await request(app.server).post("/orgs").send({
+        adress: "Rua do meio",
+        CEP: "182024000",
+        city: "americo",
+        email: "test@gmail.com",
+        name: "Guilherme",
+        password: "123456",
+        phone: "168849405"
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
